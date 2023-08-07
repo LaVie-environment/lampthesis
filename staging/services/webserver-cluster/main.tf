c88ebba385e761db2369130e41a8a97cfe2b46be
armzig <bellotaophyc@outlook.com>
2023-08-07 17:58:05 +0100

    comment out the server_port

--- staging/services/webserver-cluster/main.tf
+++ staging/services/webserver-cluster/main.tf
@@ -89,7 +89,7 @@ data "template_file" "user_data" {
   template = file("user-data.sh")
 
   vars = {
-    server_port = var.server_port
+    #server_port = var.server_port
     db_address = data.terraform_remote_state.db.outputs.address
     db_port = data.terraform_remote_state.db.outputs.port
   }
