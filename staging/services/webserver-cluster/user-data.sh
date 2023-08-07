b6d1d83bdf8a0e32027488132566e6e71cba9726
armzig <bellotaophyc@outlook.com>
2023-08-07 17:10:24 +0100

    correct the user-data script

--- staging/services/webserver-cluster/user-data.sh
+++ staging/services/webserver-cluster/user-data.sh
@@ -5,5 +5,5 @@ cat > index.html <<EOF
 <h1>Hello, this is a basic Panta page served by Nginx</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
-sudo service nginx restart
 EOF
+sudo service nginx restart
